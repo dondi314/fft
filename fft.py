c71a414000955faf93ff79d986406e316760d469
Dondi Ellis <dondi@umich.edu>
2018-05-05 17:01:36 -0400

    Update fft.py

--- fft.py
+++ fft.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def hadamard(x, y):
     return [a * b for a, b in zip(x, y)]
 
@@ -6,7 +8,9 @@ def vec_add(x, y):
     return [a + b for a, b in zip(x, y)]
 
 
-def fft(x):
+def fft(x, inverse=False):
+    if inverse:
+        return ifft(x)
     assert len(x) & (len(x) - 1) == 0, "length of x must be power of 2"
     EXP = 2.7182818284590452353602874713526624977
     PI = 3.1415926535897932384626433832795028841
@@ -32,13 +36,42 @@ def ifftshift(x):
     return x[len(x) // 2:] + x[:len(x) // 2]
 
 
-def convolve(x, y):
-    length = len(x) + len(y) - 1
-    x = x + [0] * (length - len(x))
-    next_prime = len(x)
-    while next_prime & (next_prime - 1):
-        next_prime += 1
-    x = x + [0] * (next_prime - len(x))
-    y = y + [0] * (len(x) - len(y))
-    return [ifft(hadamard(fft(x), fft(y)))[-i]
-            for i in range(length)]
+def transform(vector, inverse=False):
+    n = len(vector)
+    if (n & (n - 1)) == 0:
+        return fft(vector, inverse)
+    else:
+        return bluestein(vector, inverse)
+
+def convolve(x, y, realoutput=True):
+    pad = len(x)
+    while pad & (pad - 1):
+        pad += 1
+    x += [0] * (pad - len(x))
+    pad = len(y)
+    while pad & (pad - 1):
+        pad += 1
+    y += [0] * (pad - len(y))
+    x = transform(x)
+    y = transform(y)
+    z = [a * b for a, b in zip(x, y)]
+    result = [transform(z, inverse=True)[-i] 
+              for i in range(len(z))]
+    if realoutput:
+        return [x.real for x in result]
+    else:
+        return result
+
+
+def bluestein(vector, inverse=False):
+    EXP = 2.7182818284590452353602874713526624977
+    PI = 3.1415926535897932384626433832795028841
+    n = len(vector)
+    m = 2**((n * 2).bit_length())
+    coef = (1j if inverse else -1j) * (PI / n)
+    exptable = [EXP ** (((i * i) % (n * 2)) * coef) for i in range(n)]
+    a = [(x * y) for x, y in zip(vector, exptable)] + [0] * (m - n)
+    b = exptable[ : n] + [0] * (m - (n * 2 - 1)) + exptable[ : 0: -1]
+    b = [x.conjugate() for x in b]
+    c = convolve(a, b, False)[ : n]
+    return [(x * y) for x, y in zip(c, exptable)]
